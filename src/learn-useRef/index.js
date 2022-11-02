a914f93c7d09b03a8965ef8c02d1bc6c99a46bc3
Ilyasa Aliadjrun <57548095+IlyasaAliadjrun@users.noreply.github.com>
2022-11-03 01:19:11 +0700

    commit tugas poin 1 ilyasa

--- src/learn-useRef/index.js
+++ src/learn-useRef/index.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import "./index.css";
 
 export default function Index(){
-    const a = 10
-    const b = 10
-    return(
-        <p>{a+b}</p>
-    )
+    const inputRef = useRef(null)
+    const [inputan, setInputan] = useState("")
+
+    function handleClick(){
+        setInputan(inputRef.current.value)
+    }
+
+    return (
+        <div className="Main">
+          <p className="Text"> Learn useRef</p>
+          <p>KELOMPOK20</p>
+
+          <input 
+            type="text"
+            ref={inputRef}
+            id="inputan"
+            name="inputan" />
+          <br/>
+          <button onClick={handleClick} type="button">Click me</button>
+          <h3>{inputan}</h3>
+
+        </div>
+    );
 }
